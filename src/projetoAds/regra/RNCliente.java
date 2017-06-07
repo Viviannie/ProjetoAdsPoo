e8d8c9b281073e3cd0f2f027c15b3d4427c78182
Annie <Annie@Annie-PC>
2017-06-07 13:26:14 -0300

    RNVendedor

--- src/projetoAds/regra/RNCliente.java
+++ src/projetoAds/regra/RNCliente.java
@@ -1,5 +1,10 @@
 package projetoAds.regra;
+import java.util.ArrayList;
+import projetoAds.DAO.DAOCliente;
+import projetoAds.DAO.DAOClienteImpl;
 import projetoAds.classesBasicas.Cliente;
+import projetoAds.excecao.ConexaoException;
+import projetoAds.excecao.DAOException;
 import projetoAds.excecao.RegraException;
 
 /**
@@ -9,6 +14,60 @@ import projetoAds.excecao.RegraException;
 public class RNCliente {
     
     //precisa validar
-    public void incluir(Cliente t) throws RegraException{
+        
+    private final DAOCliente dao = new DAOClienteImpl();
+
+    public void validar(Cliente c) throws RegraException {
+        if ((c.getCli_cpf() == null) || (c.getCli_nome().trim().equals(" "))) {
+            throw new RegraException("Nome inválido");
+        }
+    }
+
+    public void verificaDuplicidade(Cliente c) throws RegraException {
+        try {
+            Cliente x = dao.pesquisar(c.getCli_cpf());
+            if (x != null) {
+                throw new RegraException("Cliente já existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
+
+    public void incluir(Cliente c) throws RegraException{
+        try {
+            dao.incluir(c);
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException();
+        }
+    }
+
+    public void excluir(Cliente c) throws RegraException {
+        if (c.getCli_cpf() == null) {
+            throw new RegraException("CPF inválido!");
+        }
+
+        try {
+            Cliente x = dao.pesquisar(c.getCli_cpf());
+            if (x == null) {
+                throw new RegraException("CPF informado não existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+
+        try {
+            dao.excluir(c);
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
+    
+    public ArrayList<Cliente> listar()throws RegraException{
+        try{
+            return dao.listar();
+        }catch(ConexaoException | DAOException e){
+            throw new RegraException(e.getMessage());
+        }        
     }
 }
--- src/projetoAds/regra/RNProduto.java
+++ src/projetoAds/regra/RNProduto.java
@@ -5,10 +5,69 @@
  */
 package projetoAds.regra;
 
+import projetoAds.DAO.DAOProduto;
+import projetoAds.DAO.DAOProdutoImpl;
+import projetoAds.classesBasicas.Produto;
+import projetoAds.excecao.RegraException;
+
 /**
  *
  * @author Annie
  */
 public class RNProduto {
-    
+
+    private final DAOProduto dao = new DAOProdutoImpl();
+
+    public void validar(Produto p) throws RegraException {
+
+        if ((p.getPrd_desc() == null) || (p.getPrd_desc().trim().equals(""))) {
+            throw new RegraException("Descrição inválida.");
+        }
+        if (p.getPrd_id() == null) {
+            throw new RegraException("ID inválido");
+        }
+        if (p.getFornecedor().get(0) == null) {
+            throw new RegraException("Turma inválida");
+        }
+    }
+
+    public void verificaDuplicidade(Aluno t) throws RegraException {
+        try {
+            Aluno x = dao.pesquisar(t.getCpf());
+            if (x != null) {
+                throw new RegraException("Aluno já existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
+
+    public void incluir(Aluno t) throws RegraException {
+        try {
+            dao.incluir(t);
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException();
+        }
+    }
+
+    public void excluir(Aluno t) throws RegraException {
+        if (t.getId() == null) {
+            throw new RegraException("ID inválido!");
+        }
+
+        try {
+            Aluno x = dao.pesquisar(t.getId());
+            if (x == null) {
+                throw new RegraException("ID informado não existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+
+        try {
+            dao.excluir(t);
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
 }
