755d34d6e170a8d527e61e09fed210e4d6369fd4
windistonematheus <windistonematheus@gmail.com>
2017-06-08 20:44:47 -0300

    Create Fabricante.java

---
+++ src/projetoAds/classesBasicas/Fabricante.java
@@ -0,0 +1,39 @@
+package projetoAds.classesBasicas;
+
+/**
+ * @author Grupo Programação Orientada a Objetos
+ */
+public class Fabricante {
+
+    private String razao;
+    private String cnpj;
+
+    /**
+     * @return the razao
+     */
+    public String getRazao() {
+        return razao;
+    }
+
+    /**
+     * @param razao the frn_razao to set
+     */
+    public void setRazao(String razao) {
+        this.razao = razao;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+}
