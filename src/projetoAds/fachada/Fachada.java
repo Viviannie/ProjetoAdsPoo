ce743aae600412404dcf2824c239266ca122bef8
aluno <aluno@pc-368.campus.local>
2017-06-07 21:37:49 -0300

    atualizar Fachada

--- src/projetoAds/fachada/Fachada.java
+++ src/projetoAds/fachada/Fachada.java
@@ -1,5 +1,10 @@
 package projetoAds.fachada;
 
+import java.util.ArrayList;
+import projetoAds.classesBasicas.Cliente;
+import projetoAds.excecao.RegraException;
+import projetoAds.regra.RNCliente;
+
 /**
  *
  * @author Annie
@@ -7,6 +12,11 @@ package projetoAds.fachada;
 public class Fachada {
     
     private static Fachada instancia;
+    private static RNCliente rnCliente;
+    
+    private Fachada(){
+        rnCliente = new RNCliente();
+    }
     
     public static Fachada getInstancia(){
         if(instancia == null){
@@ -15,5 +25,35 @@ public class Fachada {
         return instancia;
     }
     
+    public void salvarCliente(Cliente c) throws RegraException{
+        rnCliente.validar(c);
+        rnCliente.verificaDuplicidade(c);
+        rnCliente.incluir(c);
+    }
     
+    public void excluirCliente(Cliente c)throws RegraException{
+        rnCliente.validaCpf(c.getCli_cpf());
+        rnCliente.excluir(c);
+    }
+    
+    public void alterarCliente(Cliente c)throws RegraException{
+        rnCliente.validar(c);
+        rnCliente.validaCpf(c.getCli_cpf());
+        rnCliente.alterar(c);
+    }
+    
+    public Cliente pesquisarClienteNome(Cliente c)throws RegraException{
+        rnCliente.validar(c);
+        return rnCliente.pesquisar(c.getCli_nome());
+    }
+    
+    public Cliente pesquisarClienteCpf(Cliente c)throws RegraException{
+        rnCliente.validaCpf(c.getCli_cpf());
+        return rnCliente.pesquisar(c.getCli_cpf());
+    }
+    
+    public ArrayList<Cliente> listarCliente()throws RegraException{
+        return rnCliente.listar();
+    }
 }
+
