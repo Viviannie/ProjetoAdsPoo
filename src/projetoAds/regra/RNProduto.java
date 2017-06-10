e0f97a2c79b9b09baefc451270177a62fe54e59d
DiegoMaylan <diego_maylan@hotmail.com>
2017-06-10 07:20:09 -0300

    Atualizar RN produto e vendedor

--- src/projetoAds/regra/RNProduto.java
+++ src/projetoAds/regra/RNProduto.java
@@ -36,7 +36,7 @@ public class RNProduto {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public void alterar(Produto p) throws RegraException {
         try {
             dao.alterar(p);
@@ -44,7 +44,7 @@ public class RNProduto {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public Produto pesquisar(Integer id) throws RegraException {
         try {
             return dao.pesquisar(id);
@@ -52,7 +52,7 @@ public class RNProduto {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public Produto pesquisar(String desc) throws RegraException {
         try {
             return dao.pesquisar(desc);
@@ -60,14 +60,71 @@ public class RNProduto {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public ArrayList<Produto> listar() throws RegraException {
         try {
             return dao.listar();
         } catch (ConexaoException | DAOException e) {
             throw new RegraException(e.getMessage());
         }
-    
+
+    }
+
+    /**
+     * Verifica se um ID passado é válido e existe no BD
+     *
+     * @param id Para validação
+     * @throws RegraException Caso o ID não seja localizado
+     */
+    public void validaId(Integer id) throws RegraException {
+        if (id == null) {
+            throw new RegraException("ID inválido!");
+        }
+        try {
+            Produto x = dao.pesquisar(id);
+            if (x == null) {
+                throw new RegraException("ID informado não existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
+
+    /**
+     * Verifica se os campos estão preenchidos corretamente
+     *
+     * @param p Objeto com os dados
+     * @throws RegraException
+     */
+    public void validar(Produto p) throws RegraException {
+        if ((p.getDesc() == null) || (p.getDesc().trim().equals(""))) {
+            throw new RegraException("Descrição inválida");
+        }
+        
+        if (p.getEstoqueAtual() == null) {
+            throw new RegraException("Estoque atual inválido");
+        }
+        
+        if (p.getEstoqueMinimo() == null) {
+            throw new RegraException("Estoque minimo inválido");
+        }
     }
     
+    /**
+     * Verifica se uma nova descrição já existe no BD
+     *
+     * @param p Objeto com os dados
+     * @throws RegraException
+     */
+    public void verificaDuplicidade(Produto p) throws RegraException {
+        try {
+            Produto x = dao.pesquisar(p.getDesc());
+            if (x != null) {
+                throw new RegraException("Produto já existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
+
 }
--- src/projetoAds/regra/RNVendedor.java
+++ src/projetoAds/regra/RNVendedor.java
@@ -36,7 +36,7 @@ public class RNVendedor {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public void alterar(Vendedor v) throws RegraException {
         try {
             dao.alterar(v);
@@ -44,7 +44,7 @@ public class RNVendedor {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public Vendedor pesquisar(Integer id) throws RegraException {
         try {
             return dao.pesquisar(id);
@@ -52,7 +52,7 @@ public class RNVendedor {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public Vendedor pesquisar(String nome) throws RegraException {
         try {
             return dao.pesquisar(nome);
@@ -60,7 +60,7 @@ public class RNVendedor {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
     public ArrayList<Vendedor> listar() throws RegraException {
         try {
             return dao.listar();
@@ -68,5 +68,54 @@ public class RNVendedor {
             throw new RegraException(e.getMessage());
         }
     }
-    
+
+    /**
+     * Verifica se um ID passado é válido e existe no BD
+     *
+     * @param id Para validação
+     * @throws RegraException Caso o ID não seja localizado
+     */
+    public void validaId(Integer id) throws RegraException {
+        if (id == null) {
+            throw new RegraException("ID inválido!");
+        }
+        try {
+            Vendedor x = dao.pesquisar(id);
+            if (x == null) {
+                throw new RegraException("ID informado não existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
+
+    /**
+     * Verifica se os campos estão preenchidos corretamente
+     *
+     * @param v Objeto com os dados
+     * @throws RegraException
+     */
+    public void validar(Vendedor v) throws RegraException {
+        if ((v.getNome() == null) || (v.getNome().trim().equals(""))) {
+            throw new RegraException("Nome inválido");
+        }
+    }
+
+    /**
+     * Verifica se um novo nome já existe no BD
+     *
+     * @param v Objeto com os dados
+     * @throws RegraException
+     */
+    public void verificaDuplicidade(Vendedor v) throws RegraException {
+        try {
+            Vendedor x = dao.pesquisar(v.getNome());
+            if (x != null) {
+                throw new RegraException("Vendedor já existe.");
+            }
+        } catch (ConexaoException | DAOException e) {
+            throw new RegraException(e.getMessage());
+        }
+    }
+
 }
