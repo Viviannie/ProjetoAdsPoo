b8425a6986cc2b0a1cabfeb82a05de20faa1c01e
aluno <aluno@pc-312.campus.local>
2017-06-10 14:19:56 -0300

    Atualização

--- src/projetoAds/classesBasicas/Cliente.java
+++ src/projetoAds/classesBasicas/Cliente.java
@@ -36,4 +36,8 @@ public class Cliente {
         this.nome = nome;
     }
 
+    public void setCpf(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
