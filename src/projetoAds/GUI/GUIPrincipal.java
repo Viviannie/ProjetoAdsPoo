cc2ff4d20db7bab01287933f3dd78334f65805bf
DiegoMaylan <diego_maylan@hotmail.com>
2017-06-20 22:55:14 -0300

    Atualizar pesquisar produto

--- src/projetoAds/GUI/GUIPrincipal.java
+++ src/projetoAds/GUI/GUIPrincipal.java
@@ -1362,16 +1362,18 @@ public class GUIPrincipal extends javax.swing.JFrame {
         try {
             produto = fachada.pesquisarProdutoId(produto);
             JOptionPane.showMessageDialog(this, "Registro encontrado com sucesso");
+            /*
             JOptionPane.showMessageDialog(this, "Descrição: " + produto.getDesc() + "\n"
                     + "Id: " + produto.getId() + "\n"
                     + "CNPJ fabricante: " + produto.getFabricante().getCnpj() + "\n"
                     + "Estoque mínimo: " + produto.getEstoqueMinimo() + "\n"
                     + "Estoque atual: " + produto.getEstoqueAtual());
-            txtDescProduto.setText(null);
-            txtFabricanteCnpj.setText(null);
-            txtIdProduto.setText(null);
-            txtEstMinProduto.setText(null);
-            txtEstAtualProduto.setText(null);
+            */
+            txtDescProduto.setText(produto.getDesc());
+            txtFabricanteCnpj.setText(produto.getFabricante().getCnpj());
+            txtIdProduto.setText(Integer.toString(produto.getId()));
+            txtEstMinProduto.setText(Integer.toString(produto.getEstoqueMinimo()));
+            txtEstAtualProduto.setText(Integer.toString(produto.getEstoqueAtual()));
         } catch (RegraException e) {
             JOptionPane.showMessageDialog(this, e.getMessage());
         }
